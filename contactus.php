122369943610070ce271129c761cfa8b31446862
Bishal Swarnkar <userboy.bishal@gmail.com>
2018-05-07 18:32:31 +0545

    Adding More Information
    Edited Contact Us Page and Added Some More Information like "Connect With Us" Section and Maps.

--- contactus.php
+++ contactus.php
@@ -49,6 +49,24 @@
 				?>
 			</div>
 		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6 contact-w3-agile1" data-aos="flip-right">
+			<h4>Connect With Us</h4>
+			<p class="contact-agile1"><strong>Phone :</strong>+977 (01)663-86-39</p>
+			<p class="contact-agile1"><strong>Email :</strong> <a href="mailto:name@example.com">INFO@CITYVIEWHOTEL.COM</a></p>
+			<p class="contact-agile1"><strong>Address :</strong> New Road, Kathmandu, Nepal</p>
+
+			<div class="social-bnr-agileits footer-icons-agileinfo">
+				<ul class="social-icons3">
+								<li><a href="#" class="fa fa-facebook icon-border facebook"> </a></li>
+								<li><a href="#" class="fa fa-twitter icon-border twitter"> </a></li>
+								<li><a href="#" class="fa fa-google-plus icon-border googleplus"> </a></li>
+
+							</ul>
+			</div>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.813548522932!2d85.31735021444383!3d27.692157032767536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19b19295555f%3A0xabfe5f4b310f97de!2sThe+British+College%2C+Kathmandu!5e0!3m2!1sen!2snp!4v1511348248407" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+		</div>
+		<div class="clearfix"></div>
+	</div>
 </section>
 <!-- /contact -->
 <?php include_once('footer.php');?>
